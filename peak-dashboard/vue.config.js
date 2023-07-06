69aa26a674a1e65e46eb359986ad5e4e44627ab9
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2023-07-06 14:24:01 +0100

    fix: Invalid Host header

--- peak-dashboard/vue.config.js
+++ peak-dashboard/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    public: 'mas.gecad.isep.ipp.pt:8080'
+  }
 })
