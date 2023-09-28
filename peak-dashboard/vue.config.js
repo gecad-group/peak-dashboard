aaeacfcaa72a48fa2073e109fb612cec85b5fec7
Bruno Ribeiro <59573315+brunus-reberes@users.noreply.github.com>
2023-09-28 16:43:08 +0100

    update

--- peak-dashboard/vue.config.js
+++ peak-dashboard/vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    public: 'mas.gecad.isep.ipp.pt:8080'
-  }
+  //devServer: {
+    //clientLogLevel: 'info',
+    //public: 'mas.gecad.isep.ipp.pt:8080'
+  //}
 })
